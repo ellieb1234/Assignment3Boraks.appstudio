54bb1601d1afa34405f35a4a9459dd6492ceb096
ellieb1234 <ellieboraks@creighton.edu>
2021-03-14 11:03:57 -0500

    cityWhile

---
+++ Assignment3Boraks.appstudio/forms/City/City.js
@@ -0,0 +1,14 @@
+let cities=[]
+let i=0
+let print=false
+while (i<=cities.length){
+  while (print!=true){
+    let city=prompt("Enter a city, if you want to end the program type x")
+    if(city!="x"){
+      cities.append(city)
+    }
+    else{
+      p
+    }
+    }
+  }
